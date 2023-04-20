7389c1f5a443fc0eb92c5b64b6216961148b27d7
Jesse Manninen <jesse.manninen@gmail.com>
2023-04-20 22:27:34 +0300

    removed srv

--- client/teamprojects/includes/head.php
+++
@@ -1,4 +0,0 @@
-<meta charset="utf-8">
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
